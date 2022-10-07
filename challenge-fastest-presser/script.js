4b87628bf25769fa29f97203faeab5d2004498aa
SaadiaElf <saadiaelfekak@gmail.com>
2022-10-07 01:22:41 +0100

    Added mainfeature

--- challenge-fastest-presser/script.js
+++ challenge-fastest-presser/script.js
@@ -1,11 +1,35 @@
-function startGame() {}
+let startBtn = document.querySelector(".start-btn");
+let lSum = document.querySelector(".l-counter");
+let sSum = document.querySelector(".s-counter");
+let lCount = 0;
+let sCount = 0;
+let time;
+
+function startGame() {
+  let input = document.getElementById("timerSet").value;
+  setTimeout(() => {
+    if (lCount > sCount) {
+      showWinner(".winner2");
+    } else {
+      showWinner(".winner1");
+    }
+  }, input * 1000);
+}
+
+function showWinner(num) {
+  let winner = document.querySelector(num);
+  winner.className = "show";
+}
 
 function keyBoardEvents(e) {
-  if (e.keyCode === 83) {
-    // On 'S' Pressed
-  } else if (e.keyCode === 76) {
-    // On 'L' Pressed
+  if (e.code === "KeyS") {
+    sCount++;
+    sSum.innerHTML = sCount;
+  } else if (e.code === "KeyL") {
+    lCount++;
+    lSum.innerHTML = lCount;
   }
 }
 
+startBtn.addEventListener("click", startGame);
 document.addEventListener("keypress", keyBoardEvents);

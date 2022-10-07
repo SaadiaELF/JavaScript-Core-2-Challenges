fcd2b29d7197e27130394580b739d0d52a927656
SaadiaElf <saadiaelfekak@gmail.com>
2022-10-07 15:40:17 +0100

    Added character counter function

--- challenge-mini-twitter/src/index.js
+++ challenge-mini-twitter/src/index.js
@@ -1,13 +1,15 @@
 let message = document.querySelector(".post-message");
 let form = document.querySelector(".form");
 let tweetsDiv = document.querySelector(".tweets");
+let countDiv = document.querySelector(".character-counter");
 let time = new Date();
+let count = 0;
 
 function postTweet() {
   let tweetDiv = document.createElement("div");
   let timeDiv = document.createElement("div");
   tweetDiv.className = "tweet";
-  tweetDiv.innerHTML = message.value.replace(/\r|\n/, "<br>");;
+  tweetDiv.innerHTML = message.value.replace(/\r|\n/, "<br>");
   timeDiv.className = "time";
   timeDiv.innerHTML =
     "- " +
@@ -15,10 +17,19 @@ function postTweet() {
       timeZone: "UTC",
     });
   tweetDiv.appendChild(timeDiv);
-  tweetsDiv.appendChild(tweetDiv);
+  if (count >= 0) {
+    tweetsDiv.appendChild(tweetDiv);
+  }
   message.value = "";
 }
 
+function characterCount(e) {
+  count = 280 - e.target.value.length;
+  countDiv.style.color = count < 0 ? "red" : "initial";
+  countDiv.innerHTML = `${count} characters left`;
+}
+
+message.addEventListener("input", characterCount);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   postTweet();

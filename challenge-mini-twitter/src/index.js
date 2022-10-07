fa3072f826586b98c4122d23189f67fc2bc810ab
SaadiaElf <saadiaelfekak@gmail.com>
2022-10-07 14:40:59 +0100

    Added time and restyled tweet div

--- challenge-mini-twitter/src/index.js
+++ challenge-mini-twitter/src/index.js
@@ -1,11 +1,21 @@
 let message = document.querySelector(".post-message");
 let form = document.querySelector(".form");
 let tweetsDiv = document.querySelector(".tweets");
+let time = new Date();
 
 function postTweet() {
-  let tweet = document.createElement("p");
-  tweet.innerHTML = message.value;
-  tweetsDiv.appendChild(tweet);
+  let tweetDiv = document.createElement("div");
+  let timeDiv = document.createElement("div");
+  tweetDiv.className = "tweet";
+  tweetDiv.innerHTML = message.value.replace(/\r|\n/, "<br>");;
+  timeDiv.className = "time";
+  timeDiv.innerHTML =
+    "- " +
+    time.toLocaleString("en-GB", {
+      timeZone: "UTC",
+    });
+  tweetDiv.appendChild(timeDiv);
+  tweetsDiv.appendChild(tweetDiv);
   message.value = "";
 }
 
